152ed7b295f7d299728c62b08a6bba4bc116548f
Paul Scott <8pscott@gmail.com>
2020-09-29 17:51:01 -0400

    mobile styles

--- gossamer/build_project.py
+++ gossamer/build_project.py
@@ -1,8 +1,11 @@
 from generate_html import HTMLGenerator
 from generate_css import CSSGenerator
+import shutil
 import sys
 
 html_gen = HTMLGenerator(sys.argv[1])
 html_gen.parse_root()
 css_gen = CSSGenerator(sys.argv[1], html_gen.components)
 css_gen.convert()
+
+shutil.copytree(sys.argv[1] + '/assets/', sys.argv[1] + '/build/assets/')
